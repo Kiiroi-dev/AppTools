0ff114200cf8074dafc4a08e2678d264f5066b49
Kiiroi-dev <afailox@gmail.com>
2021-05-31 13:41:33 +0200

    Enregistrement DB de nouveau OK. A faire : Lire le projet test dans la db et afficher dans carview.

--- app/src/main/java/com/example/apptools/ui/finance/FinanceFragment.kt
+++ app/src/main/java/com/example/apptools/ui/finance/FinanceFragment.kt
@@ -5,16 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.apptools.R
+import com.example.apptools.Tools.RecyclerAdapter
 import com.example.apptools.databinding.FragmentFinanceBinding
+import com.example.apptools.ui.finance.data.EntityBudget
 import com.example.apptools.ui.finance.ui.FinanceViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FinanceFragment : Fragment(R.layout.fragment_finance) {
 
     private lateinit var financeViewModel: FinanceViewModel
-    private var _binding: FragmentFinanceBinding? = null
+
+
 
 
     // This property is only valid between onCreateView and
@@ -25,17 +32,26 @@ class FinanceFragment : Fragment(R.layout.fragment_finance) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val recyclerView: RecyclerView? = view?.findViewById(R.id.recycler_view)
+        val adapter = RecyclerAdapter()
+        recyclerView?.adapter = adapter
+        recyclerView?.layoutManager = LinearLayoutManager(requireContext())
+
+
+        financeViewModel = ViewModelProvider(this).get(FinanceViewModel::class.java)
+        financeViewModel.readAllData.observe(viewLifecycleOwner, Observer {EntityBudget ->
+            adapter.setData(EntityBudget)
+        } )
+
         val view = inflater.inflate(R.layout.fragment_finance, container, false)
         var btnAddBudget: FloatingActionButton? = view?.findViewById(R.id.floatingActionButton)
         btnAddBudget?.setOnClickListener {
             findNavController().navigate(R.id.action_nav_finance_to_nav_formAddBudget)
         }
+
         return view
 
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
 }
